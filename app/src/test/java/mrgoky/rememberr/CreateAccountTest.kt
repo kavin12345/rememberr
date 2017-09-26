21890476f4c22e8c4cfd103367c0b9632f0e48a0
Kevin Seo <seokev@gmail.com>
2017-09-25 18:19:40 -0700

    wip

---
+++ app/src/test/java/mrgoky/rememberr/CreateAccountTest.kt
@@ -0,0 +1,13 @@
+package mrgoky.rememberr
+
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class CreateAccountTest {
+    @Before
+    fun setup() {
+        
+    }
+}
